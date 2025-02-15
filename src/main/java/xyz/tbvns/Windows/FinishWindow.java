fa7f908e08c21b6f85d37013b8488438f2feca8a
____tbvns____ <tbvns601@gmail.com>
2025-02-15 01:27:00 +0100

    Added some JVM args to allow for cross-app communication. V2

--- src/main/java/xyz/tbvns/Windows/FinishWindow.java
+++ src/main/java/xyz/tbvns/Windows/FinishWindow.java
@@ -72,14 +72,26 @@ public class FinishWindow {
             }
             if (start.isEnabled()) {
                 try {
-                    ProcessBuilder builder = new ProcessBuilder();
-                    builder.command(Constant.javaBinLocation, "-jar", Constant.resFolder + "/bin/AppStore.jar");
-                    builder.start();
-//                    Runtime.getRuntime().exit(0);
+                    new ProcessBuilder()
+                        .command(
+                            Constant.javaBinLocation,
+                            "--add-exports=java.base/jdk.internal.ref=ALL-UNNAMED",
+                            "--add-exports=java.base/sun.nio.ch=ALL-UNNAMED",
+                            "--add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED",
+                            "--add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+                            "--add-opens=jdk.compiler/com.sun.tools.javac=ALL-UNNAMED",
+                            "--add-opens=java.base/java.lang=ALL-UNNAMED",
+                            "--add-opens=java.base/java.lang.reflect=ALL-UNNAMED",
+                            "--add-opens=java.base/java.io=ALL-UNNAMED",
+                            "--add-opens=java.base/java.util=ALL-UNNAMED",
+                            "-jar",
+                            Constant.resFolder + "/bin/AppStore.jar"
+                        ).start();
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
             }
+
             Runtime.getRuntime().exit(0);
         });
 
